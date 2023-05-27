52b469e04827b69332a568af15ae39a530235932
raypratidina <raypz642@gmail.com>
2023-05-27 11:04:16 +0700

    membuat persistences-dao

---
+++ app/src/main/java/ppm/b/kelompok4/tokoelektronik/persistences/PeriferalDao.kt
@@ -0,0 +1,30 @@
+package ppm.b.kelompok4.tokoelektronik.persistences
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import ppm.b.kelompok4.tokoelektronik.model.Komputer
+import ppm.b.kelompok4.tokoelektronik.model.Periferal
+
+@Dao
+interface PeriferalDao {
+    @Query("SELECT * FROM Periferal")
+    fun loadAll(): LiveData<List<Periferal>>
+
+    @Query("SELECT * FROM Periferal")
+    suspend fun getList(): List<Periferal>
+
+    @Query("SELECT * FROM Periferal WHERE id = :id")
+    suspend fun find(id: String): Periferal?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg items: Periferal)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(items: List<Periferal>)
+
+    @Delete
+    fun delete(item: Periferal)
+
+    @Query("DELETE FROM Periferal WHERE id = :id")
+    fun delete(id: String)
+}
