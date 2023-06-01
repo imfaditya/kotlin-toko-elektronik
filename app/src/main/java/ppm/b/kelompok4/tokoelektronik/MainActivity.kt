bb6c2973eeb4815fb17d05894650c3ab46908bf1
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-06-01 10:34:32 +0700

    call MainScreen on MainActivity

--- app/src/main/java/ppm/b/kelompok4/tokoelektronik/MainActivity.kt
+++ app/src/main/java/ppm/b/kelompok4/tokoelektronik/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import ppm.b.kelompok4.tokoelektronik.screens.MainScreen
 import ppm.b.kelompok4.tokoelektronik.ui.theme.TokoElektronikTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +25,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TokoElektronikTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
